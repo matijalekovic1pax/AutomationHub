03e01375e9a06ef1c6a47aef7937a8d444c39454
lm220148d <lm220148d@student.etf.bg.ac.rs>
2025-12-04 16:58:05 +0100

    cors fix

--- main.py
+++ main.py
@@ -346,10 +346,7 @@ app = FastAPI(title="Revit Automation Hub API", version="2.0.0", lifespan=lifesp
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
+        "https://automationhubbackend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
